86f0a1261294e71bd544b9dc4d1ca033573da2c3
itexpert120 <itexpert120@gmail.com>
2020-08-31 06:30:16 +0200

    changes for q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/gprimeltezt
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_gprimeltezt.mk \
-	$(LOCAL_DIR)/pa_gprimeltezt.mk \
 	$(LOCAL_DIR)/lineage_gprimeltezt.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_gprimeltezt-eng \
+	lineage_gprimeltezt-userdebug \
+	lineage_gprimeltezt-user
--- system_prop.mk
+++ system_prop.mk
@@ -5,8 +5,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # LCD
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=240
-
-# RAM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=false \
-	ro.config.zram=false
